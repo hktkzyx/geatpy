afc9206d0b43949a2e17af33f348c19dee7f0ef8
Brooks YUAN <hktkzyx@yeah.net>
2022-05-23 17:13:14 +0800

    refactor(benchmarks): change problem parameters in benchmarks

--- geatpy/benchmarks/mops/DTLZ3.py
+++ geatpy/benchmarks/mops/DTLZ3.py
@@ -8,14 +8,14 @@ class DTLZ3(ea.Problem):  # 继承Problem父类
 
     def __init__(self, M=3, Dim=None):  # M : 目标维数；Dim : 决策变量维数
         name = 'DTLZ3'  # 初始化name（函数名称，可以随意设置）
-        maxormins = [1] * M  # 初始化maxormins（目标最小最大化标记列表，1：最小化该目标；-1：最大化该目标）
+        maxormins = 1  # 初始化maxormins（目标最小最大化标记列表，1：最小化该目标；-1：最大化该目标）
         if Dim is None:
             Dim = M + 9  # 初始化Dim（决策变量维数）
-        varTypes = [0] * Dim  # 初始化varTypes（决策变量的类型，0：实数；1：整数）
-        lb = [0] * Dim  # 决策变量下界
-        ub = [1] * Dim  # 决策变量上界
-        lbin = [1] * Dim  # 决策变量下边界（0表示不包含该变量的下边界，1表示包含）
-        ubin = [1] * Dim  # 决策变量上边界（0表示不包含该变量的上边界，1表示包含）
+        varTypes = 0  # 初始化varTypes（决策变量的类型，0：实数；1：整数）
+        lb = 0  # 决策变量下界
+        ub = 1  # 决策变量上界
+        lbin = 1  # 决策变量下边界（0表示不包含该变量的下边界，1表示包含）
+        ubin = 1  # 决策变量上边界（0表示不包含该变量的上边界，1表示包含）
         # 调用父类构造方法完成实例化
         ea.Problem.__init__(self,
                             name,
--- geatpy/benchmarks/sops/Shubert.py
+++ geatpy/benchmarks/sops/Shubert.py
@@ -10,12 +10,12 @@ class Shubert(ea.Problem):  # 继承Problem父类
         name = 'Shubert'  # 初始化name（函数名称，可以随意设置）
         M = 1  # 初始化M（目标维数）
         Dim = 2  # 初始化Dim（决策变量维数）
-        maxormins = [1] * M  # 初始化maxormins（目标最小最大化标记列表，1：最小化该目标；-1：最大化该目标）
-        varTypes = [0] * Dim  # 初始化varTypes（决策变量的类型，0：实数；1：整数）
-        lb = [-10] * Dim  # 决策变量下界
-        ub = [10] * Dim  # 决策变量上界
-        lbin = [1] * Dim  # 决策变量下边界（0表示不包含该变量的下边界，1表示包含）
-        ubin = [1] * Dim  # 决策变量上边界（0表示不包含该变量的上边界，1表示包含）
+        maxormins = 1  # 初始化maxormins（目标最小最大化标记列表，1：最小化该目标；-1：最大化该目标）
+        varTypes = 0  # 初始化varTypes（决策变量的类型，0：实数；1：整数）
+        lb = -10  # 决策变量下界
+        ub = 10  # 决策变量上界
+        lbin = 1  # 决策变量下边界（0表示不包含该变量的下边界，1表示包含）
+        ubin = 1  # 决策变量上边界（0表示不包含该变量的上边界，1表示包含）
         # 调用父类构造方法完成实例化
         ea.Problem.__init__(self,
                             name,
